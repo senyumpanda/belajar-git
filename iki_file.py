a82716b0407618a01aebf27884d7979894155463
Ilham Pandu Prasetyo <72228596+senyumpanda@users.noreply.github.com>
2021-09-06 13:27:08 +0700

    Update iki_file.py

--- iki_file.py
+++ iki_file.py
@@ -1,18 +1,1 @@
-class Say:
-	def __init__(self):
-		res = self.kata()
-		Result(res)
-	
-	def kata(self):
-		return "Hello"	
-
-class Result:
-	def __init__(self, word):
-		res = self.print(word)
-		print(res)
-
-	def print(self, word):
-		return word + " World"
-
-
-Say()
+print("Hello World")
